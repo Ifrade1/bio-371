62506a70bf4e20bbac0254e708831716782541e4
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-03-06 15:31:30 -0500

    working on snippet for nested orfs
    still not done, just putting it here for safe keeping

---
+++ nestedsnippet.java
@@ -0,0 +1,42 @@
+   String currentORFNested;
+String ORFSNested = new String();
+   for (int q=0; q < ORFA.size(); q++) {
+       currentORFNested = ORFA.get(q);
+            //System.out.println(i);
+            stringLen = currentORFNested.length;
+            for (int r = 3; stringLen-r < minsize; r++){ //go through string
+                 if (currentORFNested.charAt(r) == 'a' || currentORFNested.charAt(r) == 'A') && (currentORFNested.charAt(r)(i+1) == 't' || currentORFNested.charAt(r)(i) == 'T') && (currentORFNested.charAt(r)(i+2)) == 'g' || currentORFNested.charAt(r)(i) == 'G') {
+                    if (r != 0 && r%3 == 0) {
+                         if (currentORFNested.charAt(r) == 't' || currentORFNested.charAt(r) == 'T') {
+                            if (((currentORFNested.charAt(r+1) == 'a'  ||currentORFNested.charAt(r+1) == 'A') && (currentORFNested.charAt(r+2) == 'g' || currentORFNested.charAt(r+2) == 'G')) || //uag
+                             ((currentORFNested.charAt(r+1) == 'a' || currentORFNested.charAt(r+1) == 'A') && currentORFNested.charAt(r+2) == 'a' || currentORFNested.charAt(r+2) == 'g') || //uaa
+                            (currentORFNested.charAt(r+1) == 'g' && currentORFNested.charAt(r+2) == 'a'))){  //uga
+                                //System.out.println("STOP codon found at " + i);
+                                ORFSNested = ORFS + "" + currentORFNested.charAt(i);
+                                ORFSNested = ORFS + "" + currentORFNested.charAt(i+1);
+                                ORFSNested = ORFS + "" + currentORFNested.charAt(i+2);
+                                //FILTER CODE
+                                  if (ORFS.length()>=minsize) {
+                                                ORFgood = ORFS;
+                                                 ORFANested.add(ORFgood);
+                                                //System.out.println("o is greater than minsize");
+                                         }
+                                else ORFgood = "";
+                                //FILTER END
+                                i = i+2;
+                                break;
+                            }
+                        }
+                    }
+                    ORFS = ORFS + "" + currentORFNested.charAt(r);
+                    i++;
+                }
+
+                if (ORFgood != "") {
+               
+                }
+
+                ORFS = "";
+                ORFgood = "";
+            }
+        }
